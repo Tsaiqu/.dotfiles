b010b73305252dd77daf93d22f39692ede4c5c19
Tsaiqu <ksasin3@gmail.com>
2026-08-11 08:28:22 +0200

    fix lta alias + add gitignore

--- zsh/.oh-my-zsh/custom/aliases.zsh
+++ zsh/.oh-my-zsh/custom/aliases.zsh
@@ -1,14 +1,14 @@
-alias lt="exa -T -L=1 --group-directories-first -F -I=node_modules"
-alias lta="exa -T -L=1 -a --group-directories-first -F -I=node_modules"
-alias la="exa -l -a --group-directories-first -I=node_modules"
-alias ls="exa --icons --group-directories-first -I=node_modules"
+alias lt="eza -T -R -L=1 --group-directories-first -F -I=node_modules"
+alias lta="eza -T -L=1 -a --group-directories-first -F -I=node_modules"
+alias la="eza -l -a --group-directories-first -I=node_modules"
+alias ls="eza --icons --group-directories-first -I=node_modules"
 
 for i in {1..9}; do
-  alias "lta${i}"="exa -T -L=${i} -a --group-directories-first -I=node_modules"
+  alias "lta${i}"="eza -T -R -L=${i} -a --group-directories-first -I=node_modules"
 done
 
 for i in {1..9}; do
-  alias "lt${i}"="exa -T -L=${i} --group-directories-first -I=node_modules"
+  alias "lt${i}"="eza -T -R -L=${i} --group-directories-first -I=node_modules"
 done
 
 # Git
